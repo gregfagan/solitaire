f3d57da3b7ea46628308ac205824bf65efbba8c8
gregfagan <gregfagan@gmail.com>
2015-06-15 17:03:49 -0700

    Fix error when looking up unmounted node
    Fix is hacky.

--- app/js/components/draganddrop.jsx
+++ app/js/components/draganddrop.jsx
@@ -49,7 +49,7 @@ export default class DragAndDrop extends React.Component {
       document.removeEventListener('mousemove', this.boundOnGlobalMouseMove);
       document.removeEventListener('mouseup', this.boundOnGlobalMouseUp);
 
-      if (React.findDOMNode(this)) {
+      if (isMounted(this)) {
         this.setState({
           dragging: false,
           offset: { x:0, y:0 }
@@ -112,3 +112,20 @@ export default class DragAndDrop extends React.Component {
 };
 
 DragAndDrop.defaultProps = { interaction: {} };
+
+//
+// this.isMounted was removed for ES6 classed React Components. React.findDOMNode
+// throws if the component isn't mounted. This function reimplements the isMounted
+// function using try/catch.
+//
+// see http://jaketrent.com/post/set-state-in-callbacks-in-react/
+//
+function isMounted(component) {
+  try {
+    React.findDOMNode(component);
+    return true;
+  }
+  catch (e) {
+    return false;
+  }
+}
