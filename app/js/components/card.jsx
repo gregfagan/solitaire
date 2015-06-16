e41cec63dc69e6ddd383a37cb88c9fbbd159530b
gregfagan <gregfagan@gmail.com>
2015-06-15 17:10:37 -0700

    ES6ify Card.jsx

--- app/js/components/card.jsx
+++ app/js/components/card.jsx
@@ -1,36 +1,33 @@
-/**
- * @jsx React.DOM
- */
+import _ from "lodash";
+import React from "react";
+import Card from "game/card";
+import classnames from 'classnames';
 
- var _ = require("lodash")
- var React = require("react/addons");
- var Card = require("game/card");
-
- var FaceView = React.createClass({
-  render: function() {
-    var id = Card.toId(this.props.card);
+class FaceView extends React.Component {
+  render() {
+    const id = Card.toId(this.props.card);
     return (
       <img width='100%' height='100%' key={`card_face_${id}`} src={Card.images[id]} />
-      );
+    );
   }
-});
+};
  
- var CardView = React.createClass({
-  render: function() {
-    var id = this.props.card ? "card_" + Card.toId(this.props.card) : "";
+ export default class CardView extends React.Component {
+  render() {
+    const id = this.props.card ? "card_" + Card.toId(this.props.card) : "";
 
-    var classes = React.addons.classSet({
+    const classes = classnames({
       'card': true,
       'flipped': this.props.flipped,
       'cascade-down': this.props.cascade === "down",
       'cascade-none': this.props.cascade === "none"
     });
 
-    var front = this.props.card ?
+    const front = this.props.card ?
       <FaceView card={this.props.card} /> :
       <div className="side slot"></div>;
 
-    var back;
+    let back;
     if (this.props.card) {
       back = <div className="side back"/>;
     }
@@ -42,6 +39,4 @@
       </div>
       );
   }
-});
-
- module.exports = CardView;
+};
